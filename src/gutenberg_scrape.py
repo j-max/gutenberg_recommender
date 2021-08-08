cc451a517e10c0499dd9b683286dcdde7a45da41
j-max <jmax.barry@gmail.com>
2021-08-08 10:39:28 -0500

    Add guttenberg scrape up to austin

--- src/gutenberg_scrape.py
+++ src/gutenberg_scrape.py
@@ -1,5 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 from bs4 import BeautifulSoup
 import requests
@@ -12,20 +10,24 @@ On August 5th, 2021, the top 8 most popular eBooks are good candidates for the p
 top_books_url = 'https://www.gutenberg.org/browse/scores/top'
 response = requests.get(top_books_url)
 soup = BeautifulSoup(response.content)
-top_100 = soup.select('h2#books-last1')
 
+# the first ordered list on this page is the top 100 books from yesterday
+top_100 = soup.find('ol')
+top_100 = top_100.find_all('li')
 
-# chromedriver = "/Applications/chromedriver" # path to the chromedriver executable
-# os.environ["webdriver.chrome.driver"] = chromedriver
-# 
-# driver = webdriver.Chrome(chromedriver)
-# 
-# driver.get('https://www.gutenberg.org/browse/scores/top')
-# 
-# '''
-# On August 5th, 2021, the top 8 most popular eBooks are good candidates for the project: From Pride and Prejudice to Frankenstein.
-# '''
-# 
-# 
-# time.sleep(2)
-# driver.close()
+top_10 = top_100[:10]
+
+# href is the book url embedded in a link
+top_10 = {book.text: 'https://www.gutenberg.org' + book.find('a')['href'] for book in top_10}
+top_10_urls = list(top_10.values())
+
+jane_austin = requests.get(top_10_urls[0])
+austin_soup = BeautifulSoup(jane_austin.content)
+
+# first table contains all the download options
+austin_table = austin_soup.find('table')
+rows = austin_table.find_all('tr')
+ebook_url = rows[1]
+
+if "Read this book online" in ebook_url.text:
+    print("Ebook found")
